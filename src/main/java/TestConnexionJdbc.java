7d0c2b606869a8cdf01759c05a36910bea8e6640
Garmi <fallgarmi83@yahoo.fr>
2023-11-19 22:05:30 +0100

    Initial commit

---
+++ src/main/java/TestConnexionJdbc.java
@@ -0,0 +1,27 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class TestConnexionJdbc {
+
+    private static final String DB_URL;
+    private static final String DB_USER;
+    private static final String DB_PWD;
+
+    static {
+        ResourceBundle bundle = ResourceBundle.getBundle("db");
+        DB_URL= bundle.getString("db.url");
+        DB_USER=bundle.getString("db.user");
+        DB_PWD=bundle.getString("db.password");
+
+   }
+
+    public static void main(String[] args) {
+        try (Connection connexion = DriverManager.getConnection(DB_URL, DB_USER, DB_PWD)){
+            System.out.println(connexion);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
