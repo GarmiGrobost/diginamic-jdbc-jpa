1a73f08c857d497131c342afe4a9511f40c5be8b
Garmi <fallgarmi83@yahoo.fr>
2023-11-21 22:31:49 +0100

    TP 3

--- src/main/java/fr/diginamic/jdbc/entites/Fournisseur.java
+++ src/main/java/fr/diginamic/jdbc/entites/Fournisseur.java
@@ -34,4 +34,12 @@ public class Fournisseur {
     public void setNom(String nom) {
         this.nom = nom;
     }
+
+    @Override
+    public String toString() {
+        return "Fournisseur{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                '}';
+    }
 }
